a4ab71192b0384a0891b59170141b513c0e290ba
Jason <jason1996429@gmail.com>
2020-02-16 01:23:36 -0500

    finalize code

--- HueControl/HueControl.ino
+++ HueControl/HueControl.ino
@@ -3,15 +3,14 @@
 #include <ArduinoHttpClient.h>
 #include "arduino_secrets.h"
 
-#define switch 4
-#define outputA 2
-#define outputB 3
+#define switch 8
 
 WiFiClient wifi;
 String onApi = "  {\"on\": true}";
 String offApi = "  {\"on\": false}";
 boolean currentState = false;
 boolean previousState = false;
+
 String hubAddress[] = {"172.22.151.185",
                        "172.22.151.181"
                       };
@@ -20,41 +19,57 @@ String hubLogin[] = {"ISr8hVdpuJmmDs-COJb-d29KQD3tlgcdocc36eiD",
                     };
 int lightNumbers[] = {3, 1};
 HttpClient httpClient = HttpClient(wifi, hubAddress[0]);
-int currentHub = 0;
+int currentHub = 1;
 
-long lastRequest[] = {10000,10000};
-int requestDelay[] = {500,1500};
+long lastRequest[] = {10000,10000,10000,10000};
+int requestDelay[] = {500,1500,1500,1500};
 
+// switching handling
 int switchState;
 int pSwitchState;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
-int ledState = LOW;         // the current state of the output pin
-int lastLedState = HIGH;
-int start = 0;
+int lightState;
+int pLightState;
 
-int counter = 0; 
-int pCounter = 0;
-int aState;
-int aLastState;
-int sentCounter = 0;
-bool sent = false;
+// rotary encoder handling
+int output[][2] = {{2,3},
+                  {4,5},
+                  {6,7}};
+int counter[] = {0,0,0}; 
+int pCounter[] = {0,0,0};
+int rotaryState[3];
+int pRotaryState[3];
+int sentCounter[] = {0,0,0};
+bool sent[] = {false,false,false};
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  // while (!Serial);
 
   pinMode(switch, INPUT);
-  pinMode (outputA,INPUT);
-  pinMode (outputB,INPUT);
-  
+  pinMode(output[0][0], INPUT);
+  pinMode(output[0][1], INPUT);
+  pinMode(output[1][0], INPUT);
+  pinMode(output[1][1], INPUT);
+  pinMode(output[2][0], INPUT);
+  pinMode(output[2][1], INPUT);
+
   switchState = digitalRead(switch);
-  aLastState = digitalRead(outputA);
   if(switchState == HIGH){
+    lightState = HIGH;
     pSwitchState = LOW;
+    pLightState = LOW;
   }else{
+    lightState = LOW;
     pSwitchState = HIGH;
+    pLightState = HIGH;
   }
+
+  pRotaryState[0] = digitalRead(output[0][0]);
+  pRotaryState[1] = digitalRead(output[1][0]);
+  pRotaryState[2] = digitalRead(output[2][0]);
+
   while ( WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(SECRET_SSID);
@@ -62,16 +77,14 @@ void setup() {
     delay(2000);
   }
 
-  // you're connected now, so print out the data:
   Serial.print("You're connected to the network. Your IP = ");
   IPAddress ip = WiFi.localIP();
   Serial.println(ip);
 }
 
 void loop() {
-  // clean up after the last set of requests by waiting for any remaining response:
   while (httpClient.connected()) {
-    // print out whatever you get:
+    // read response if available
     if (httpClient.available() > 0) {
       String response = httpClient.readString();
       Serial.println(response);
@@ -79,54 +92,43 @@ void loop() {
     }
   }
 
-  // read the state of the switch into a local variable:
   int reading = digitalRead(switch);
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != pSwitchState && start > 0) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-    start++;
-  }
-
+  // debounce handling
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-    // if the button state has changed:
     if (reading != switchState) {
       switchState = reading;
-
-      // only toggle the LED if the new button state is HIGH
+      // only toggle if the switch in on
       if (switchState == HIGH) {
-        ledState = HIGH;
+        lightState = HIGH;
       }else{
-        ledState = LOW;
+        lightState = LOW;
       }
     }
   }
-  
-  if(ledState != lastLedState){
-    // set the LED:
+  //
+  if(lightState != pLightState){
+    // wait till switch signal is stable
     if (millis() - lastRequest[0] > requestDelay[0]) {
       httpClient = HttpClient(wifi, hubAddress[currentHub]);
-      // make request:
-  //    Serial.println(ledState);
-      if(ledState == 1){
+      if(lightState == HIGH){
         Serial.println(onApi);
         sendRequest(currentHub, lightNumbers[currentHub], onApi);
-      }else if(ledState == 0){
+      }else if(lightState == LOW){
         Serial.println(offApi);
         sendRequest(currentHub, lightNumbers[currentHub], offApi);
       }
-        
-      // update the last request timestamp:
+      // update last request timestamp:
       lastRequest[0] = millis();
     }
-    lastLedState = ledState;
+    // update previous light state
+    pLightState = lightState;
   }
-  readRotary();
+
+  // read all 3 rotary encoders
+  readRotary(0, "bri", 254);
+  readRotary(1, "hue", 65535);
+  readRotary(2, "sat", 254);
   pSwitchState = reading;
-  
 }
 
 void sendRequest(int hubNumber, int light, String myState) {
@@ -151,44 +153,63 @@ void sendRequest(int hubNumber, int light, String myState) {
   httpClient.put(request, contentType, body);
 }
 
-void readRotary(){
-  aState = digitalRead(outputA); // Reads the "current" state of the outputA
-  // If the previous and the current state of the outputA are different, that means a Pulse has occured
-  if (aState != aLastState){ 
-    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(outputB) != aState) { 
-      counter ++;
+void readRotary(int index, String prop, int cap){
+  rotaryState[index] = digitalRead(output[index][0]);
+  // detect rotary encoder state change
+  if (rotaryState[index] != pRotaryState[index]){ 
+    // If the another pin state is different to one pin, encoder is rotated clockwise
+    if (digitalRead(output[index][1]) != rotaryState[index]) { 
+      if(prop != "hue"){
+        counter[index] ++;
+      }else if(prop == "hue"){
+        // special handing for huge hue value
+        counter[index] = counter[index] + 100;
+      }
     } else {
-      counter --;
+      if(prop != "hue"){
+        counter[index] --;
+      }else if(prop == "hue"){
+        counter[index] = counter[index] - 100;
+      }
     }
-    if(counter < 2 || counter <= 0){
-      counter = 1;
+
+    // restrict floor
+    if(counter[index] < 2 || counter[index] <= 0){
+      counter[index] = 1;
     }
-    if(counter > 254){
-      counter = 254;
+    // restrict ceiling
+    if(counter[index] > cap){
+      counter[index] = cap;
     }
-    Serial.print("Position: ");
-    Serial.println(counter);
-    sent = false;
+
+    // update sent status
+    sent[index] = false;
   }
-  if(aState == aLastState){
-    if (millis() - lastRequest[1] > requestDelay[1]) {
-      if(sentCounter != counter && sent == false){
+
+  // if not rotating
+  if(rotaryState[index] == pRotaryState[index]){
+    // wait till the rotary encoder state to be stable
+    if (millis() - lastRequest[index + 1] > requestDelay[index+1]) {
+      // if value is new and not being sent
+      if(sentCounter[index] != counter[index] && sent[index] == false){
         Serial.println("fire");
         httpClient = HttpClient(wifi, hubAddress[currentHub]);
-        sendRequest(currentHub, lightNumbers[currentHub], lightBrightness(counter));
-        sent = true;
-        sentCounter = counter;
+        sendRequest(currentHub, lightNumbers[currentHub], propHelper(prop, counter[index]));
+        // update sent status
+        sent[index] = true;
+        sentCounter[index] = counter[index];
       }
-      // update the last request timestamp:
-      lastRequest[1] = millis();
+      // update last request timestamp:
+      lastRequest[index+1] = millis();
     }
   }
- aLastState = aState;
+ pRotaryState[index] = rotaryState[index];
 }
 
-String lightBrightness(int brightness){
-    String state = "  {\"bri\": num}";
-    state.replace("num", String(brightness));
+// helper function for composing api body
+String propHelper(String prop, int num){
+    String state = "  {\"prop\": num}";
+    state.replace("prop", prop);
+    state.replace("num", String(num));
     return state;
 }
